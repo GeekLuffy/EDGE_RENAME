a93ab83da0fc0f23541ae42f9a1284a6ff275c93
GeekLuffy <71143694+GeekLuffy@users.noreply.github.com>
2023-08-20 12:09:11 +0530

    Update Force_Sub.py

--- plugins/Force_Sub.py
+++ plugins/Force_Sub.py
@@ -1,42 +1,22 @@
-"""
-Apache License 2.0
-Copyright (c) 2022 @PYRO_BOTZ 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-Telegram Link : https://t.me/PYRO_BOTZ 
-Repo Link : https://github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT
-License Link : https://github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT/blob/main/LICENSE
-"""
-
-from pyrogram import Client, filters, enums 
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client, enums, filters
 from pyrogram.errors import UserNotParticipant
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
 from config import Config
 from helper.database import db
 
+
 async def not_subscribed(_, client, message):
     await db.add_user(client, message)
-    if not Config.FORCE_SUB:
+    if not Config.FORCE_SUB_1 and Config.FORCE_SUB_2:
         return False
-    try:             
-        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id) 
-        if user.status == enums.ChatMemberStatus.BANNED:
-            return True 
+    try:
+        user1 = await client.get_chat_member(Config.FORCE_SUB_1, message.from_user.id)
+        user2 = await client.get_chat_member(Config.FORCE_SUB_2, message.from_user.id)
+        if user1.status == enums.ChatMemberStatus.BANNED and user2.status == enums.ChatMemberStatus.BANNED:
+            return True
         else:
-            return False                
+            return False
     except UserNotParticipant:
         pass
     return True
@@ -44,17 +24,45 @@ async def not_subscribed(_, client, message):
 
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def forces_sub(client, message):
-    buttons = [[InlineKeyboardButton(text="⚡️ Join ⚡️", url=f"https://t.me/{Config.FORCE_SUB}") ]]
-    text = """**Sᴏʀʀy Dᴜᴅᴇ Yᴏᴜ'ʀᴇ Nᴏᴛ Jᴏɪɴᴇᴅ My Cʜᴀɴɴᴇʟ.
-    Sᴏ Pʟᴇᴀꜱᴇ Jᴏɪɴ Oᴜʀ Uᴩᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ Tᴏ Cᴏɴᴛɪɴᴜᴇ **"""
+    user_firstname = message.from_user.first_name
+
+    buttons = [
+        [
+            InlineKeyboardButton(text="⚡️ Join Channel 1 ⚡️", url=f"https://t.me/{Config.FORCE_SUB_1}"),
+            InlineKeyboardButton(text="⚡️ Join Channel 2 ⚡️", url=f"https://t.me/{Config.FORCE_SUB_2}")
+        ],
+        [
+            InlineKeyboardButton(text="Verify Membership", callback_data="check_joined")
+        ]
+    ]
+
+    text = f"""**Hold on, {user_firstname}! You're missing out on some serious action.
+To unleash my full power and access all the premium features, you've got to join both of our electrifying update channels below:**"""
+
     try:
-        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)    
-        if user.status == enums.ChatMemberStatus.BANNED:                                   
-            return await client.send_message(message.from_user.id, text="Sᴏʀʀy Yᴏᴜ'ʀᴇ Bᴀɴɴᴇᴅ Tᴏ Uꜱᴇ Mᴇ")  
-    except UserNotParticipant:                       
+        user_channel_1 = await client.get_chat_member(Config.FORCE_SUB_1, message.from_user.id)
+        user_channel_2 = await client.get_chat_member(Config.FORCE_SUB_2, message.from_user.id)
+
+        if (user_channel_1.status == enums.ChatMemberStatus.BANNED) and (
+                user_channel_2.status == enums.ChatMemberStatus.BANNED):
+            return await client.send_message(message.from_user.id, text="Sorry, You're Banned to Use Me")
+    except UserNotParticipant:
         return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
+
     return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
-          
 
 
+@Client.on_callback_query(filters.regex("check_joined"))
+async def check_joined_callback(client, query):
+    user = query.from_user
 
+    try:
+        user_channel_1 = await client.get_chat_member(Config.FORCE_SUB_1, user.id)
+        user_channel_2 = await client.get_chat_member(Config.FORCE_SUB_2, user.id)
+    except UserNotParticipant:
+        await query.answer("You must join both channels to access my premium features.",show_alert=True)
+        return
+    if user_channel_1.status == enums.ChatMemberStatus.MEMBER and user_channel_2.status == enums.ChatMemberStatus.MEMBER:
+        await query.answer("You have joined both Channels! Now you can freely use me", show_alert=True)
+    else:
+        await query.answer("You must join both channels to access my premium features.", show_alert=True)
