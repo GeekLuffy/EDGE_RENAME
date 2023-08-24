2c9610f5e2a4b91e6c889abfbc7ff3319ced037d
GeekLuffy <71143694+GeekLuffy@users.noreply.github.com>
2023-08-24 15:20:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ class Config(object):
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1350488685').split()]
     FORCE_SUB_1 = os.environ.get("FORCE_SUB_1", "EdgeBotSupport")
     FORCE_SUB_2 = os.environ.get("FORCE_SUB_2", "Edge_Bot_Updates")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", None))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1001863937035))
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
