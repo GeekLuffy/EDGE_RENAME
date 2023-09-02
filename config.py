e12c4fb3140fbc21db591abbcdc999032d5fd30f
GeekLuffy <71143694+GeekLuffy@users.noreply.github.com>
2023-09-02 21:13:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ class Config(object):
     # other configs
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://te.legra.ph/file/2a534b25a88d4c2e2b108.jpg")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1350488685').split()]
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1350488685' '5715764478').split()]
     FORCE_SUB_1 = os.environ.get("FORCE_SUB_1", "EdgeBotSupport")
     FORCE_SUB_2 = os.environ.get("FORCE_SUB_2", "EdgeBots")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1001863937035))
